caa13a794a61313bff3c5b33fdca77ba56e6a170
Aravinth R <122765949+aravinth-webdev@users.noreply.github.com>
2025-04-18 04:02:32 +0100

    updated

---
+++ Food-Order-main/src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { Fragment } from "react";
+
+import mealImage from "../../assests/meals.jpg";
+import classes from "./Header.module.css";
+
+import Button from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1> FoodMeals </h1> <Button onClick={props.onShowCart} />{" "}
+      </header>{" "}
+      <div className={classes["main-image"]}>
+        <img src={mealImage} alt="image" />
+      </div>{" "}
+    </Fragment>
+  );
+};
+
+export default Header;
